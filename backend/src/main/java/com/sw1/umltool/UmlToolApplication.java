325460144564b65b8f58183090e68d923a95c5f8
Stefy127 <noeste1234@gmail.com>
2026-09-03 23:48:48 -0400

    feat: complete project architecture and docker infrastructure

---
+++ backend/src/main/java/com/sw1/umltool/UmlToolApplication.java
@@ -0,0 +1,12 @@
+package com.sw1.umltool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmlToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UmlToolApplication.class, args);
+    }
+}
