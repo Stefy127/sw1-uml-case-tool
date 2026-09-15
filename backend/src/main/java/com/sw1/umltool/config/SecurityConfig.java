0c12aa9aca483117122e963d3c353ff7b526eeb5
AngelMoraRivera <angel.mora@geacco.com>
2026-09-15 16:40:26 -0500

    feat: add cors

--- backend/src/main/java/com/sw1/umltool/config/SecurityConfig.java
+++ backend/src/main/java/com/sw1/umltool/config/SecurityConfig.java
@@ -50,7 +50,11 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(
+                "http://localhost:4200",
+                "http://18-226-234-229.sslip.io",
+                "https://18-226-234-229.sslip.io"
+            )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
